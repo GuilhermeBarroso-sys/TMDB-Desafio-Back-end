3aae38351bbaebac599acc94cb6684f4259861d2
GuilhermeBarroso <202010422@unifoa.edu.br>
2022-01-06 09:43:57 -0300

    Fix env port

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ import cors from 'cors';
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
 });
